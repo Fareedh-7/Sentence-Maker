3a1c589df884b43119405de126b117c283db64d0
Fareedh-7 <fareedhahamed07@gmail.com>
2025-03-23 18:43:49 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+let adjective;
+let noun;
+let verb;
+let place;
+let adjective2;
+let noun2;
+adjective = "big";
+noun = "dog";
+verb = "ran";
+place = "park";
+adjective2 = "small";
+noun2 = "cat";
+let firstStory;
+firstStory = "The " + adjective + " " + noun + " " + verb + " to the " + place + " to catch the " + adjective2 + " " + noun2 + ".";
+console.log("First story: " + firstStory);
+adjective = "";
+noun = "";
+verb = "";
+place = "";
+adjective2 = "";
+noun2 = "";
+let secondStory;
+secondStory = "";
+console.log("Second story: " + secondStory);
+
+
